29d4fee2eb6e0c39d31e433df9218f24d51575b1
Natalia Fenclová <natalia.fenclova@keemail.me>
2018-02-07 23:06:21 +0100

    boot: add multiboot check

--- src/boot.asm
+++ src/boot.asm
@@ -6,6 +6,7 @@ global _start
 OK equ 0x2f4b2f4f
 
 section .multiboot_header
+
 header_start:
 .magic:
     dd 0xe85250d6
@@ -21,10 +22,35 @@ header_start:
     dd 8
 header_end:
 
+section .bss
+
+stack_bottom:
+  resb 64
+stack_top:
+
 section .text
 bits 32
-_start:
-  mov dword [0xb8000], OK
-  call kernel_main
-  cli
+
+_panic:
+  mov dword [0xb8000], 0x4f524f45
+  mov dword [0xb8004], 0x4f3a4f52
+  mov dword [0xb8008], 0x4f204f20
+  mov byte  [0xb800a], al
   hlt
+
+_start:
+  mov esp, stack_top
+  mov dword [0xb8000], 0x2f4b2f4f
+
+  call check_loaded_by_multiboot
+
+  call kernel_main
+  hlt
+
+check_loaded_by_multiboot:
+  cmp eax, 0x36d76289
+  jne .error
+  ret
+.error:
+  mov al, "m"
+  jmp _panic
