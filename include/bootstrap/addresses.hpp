4ae8d3d47010c16f17c295a9b82b5639fd33efeb
Natalia Fenclová <natalia.fenclova@keemail.me>
2018-02-20 21:32:47 +0100

    bootstrap: add interface to kernel load addresses

---
+++ include/bootstrap/addresses.hpp
@@ -0,0 +1,48 @@
+#ifndef JESSY_BOOTSTRAP_ADDRESSES_HPP
+#define JESSY_BOOTSTRAP_ADDRESSES_HPP
+
+#include "core/int_types.hpp"
+
+namespace os::bootstrap
+  {
+
+  extern "C" os::core::byte_t const _start_physical;
+  extern "C" os::core::byte_t const _end_physical;
+  extern "C" os::core::byte_t const _start_virtual;
+  extern "C" os::core::byte_t const _end_virtual;
+
+  /**
+   * The physical address the kernel has been loaded at
+   *
+   * @author Natalia Fenclová
+   * @since 0.1.0
+   */
+  auto const cPhysicalMemoryStartAddress = &_start_physical;
+
+  /**
+   * The physical end address of the kernel image
+   *
+   * @author Natalia Fenclová
+   * @since 0.1.0
+   */
+  auto const cPhysicalMemoryEndAddress = &_end_physical;
+
+  /**
+   * The virtual address the kernel has been loaded at
+   *
+   * @author Natalia Fenclová
+   * @since 0.1.0
+   */
+  auto const cVirtualMemoryStartAddress = &_start_virtual;
+
+  /**
+   * The virtual end address of the kernel image
+   *
+   * @author Natalia Fenclová
+   * @since 0.1.0
+   */
+  auto const cVirtualMemoryEndAddress = &_end_virtual;
+
+  }
+
+#endif
