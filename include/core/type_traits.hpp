c574c6fdf8b49458ce4d0db285bdfd50f331b0b3
Natalia Fenclová <natalia.fenclova@keemail.me>
2018-03-10 11:21:21 +0100

    type_traits: add doc for remove_reference

--- include/core/type_traits.hpp
+++ include/core/type_traits.hpp
@@ -4,9 +4,18 @@
 namespace os::core
   {
 
+  /**
+   * Calculate the type resulting from removing the ref-qualifier from the given type
+   */
   template<typename Type> struct remove_reference { using type = Type; };
   template<typename Type> struct remove_reference<Type &> { using type = Type; };
   template<typename Type> struct remove_reference<Type &&> { using type = Type; };
+
+  /**
+   * Calculate the type resulting from removing the ref-qualifier from the given type
+   *
+   * @note This is a convenience alias for os::core::remove_reference<Type>::type
+   */
   template<typename Type> using remove_reference_t = typename remove_reference<Type>::type;
 
   }
